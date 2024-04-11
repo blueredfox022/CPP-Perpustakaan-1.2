e039dd6b7a79ed38e69d2e2051ce9656dcd8b672
blueredfox022 <zulkiflisaleh022@gmail.com>
2024-04-12 03:48:11 +0700

    first commit

--- src/Pinjam.hpp
+++ src/Pinjam.hpp
@@ -8,17 +8,11 @@ struct Peminjaman
     Perpustakaan bukuPeminjam;
 };
 typedef Peminjaman ArrPeminjam[MAXSIZE];
-
 int MenuPinjam();
-
 int getDataSizePeminjam(ArrPeminjam &peminjam);
-
 void TambahDatapeminjam(ArrPeminjam &peminjam, ArrPerpustakaan &perpustakaan);
-
 Peminjaman ReadDatapeminjam(ArrPeminjam &peminjaman, int pos);
-
 void LihatDaftarPeminjam(ArrPeminjam &peminjaman);
-
 void PinjamBuku(ArrPeminjam &peminjaman, ArrPerpustakaan &perpustakaan);
 
 #endif
